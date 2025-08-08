63c66934aed48f41558fccc79ff65ce4b910ac7f
Mateo <mateo.avalos3942@gmail.com>
2025-08-08 14:54:04 -0500

    Added try-catch statements to basic CRUD

--- prueba_appu/documents/dynamodb.py
+++ prueba_appu/documents/dynamodb.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 from django.conf import settings
 
 dynamodb = boto3.resource(
@@ -11,30 +12,47 @@ dynamodb = boto3.resource(
 table = dynamodb.Table(settings.DYNAMODB_TABLE_NAME)
 
 def create_document(data:dict) -> dict:
-    table.put_item(Item=data)
-    return data
+    try:
+        response = table.put_item(Item=data)
+        return response
+    except ClientError as e:
+        raise e
 
 def get_document_by_id(document_id: str) -> dict:
-    response = table.get_item(Key={'id': document_id})
-    return response.get('Item', {})
+    try:
+        response = table.get_item(Key={'id': document_id})
+        return response.get('Item', {})
+    except ClientError as e:
+        raise e
 
 def get_documents_by_name(name: str) -> list:
-    response = table.query(
-        IndexName='file_name-index',
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('file_name').eq(name)
-    )
-    return response.get('Items', [])
+    try:    
+        response = table.query(
+            IndexName='file_name-index',
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('file_name').eq(name)
+        )
+        return response.get('Items', [])
+    except ClientError as e:
+        raise e
 
 def update_document(document_id: str, updates: dict) -> dict:
     update_expression = "SET " + ", ".join(f"{k}=:{k}" for k in updates.keys())
     expression_values = {f":{k}": v for k, v in updates.items()}
-    table.update_item(
-        Key={'id': document_id},
-        UpdateExpression=update_expression,
-        ExpressionAttributeValues=expression_values
-    )
-    return get_document_by_id(document_id)
+
+    try:
+        response = table.update_item(
+            Key={'id': document_id},
+            UpdateExpression=update_expression,
+            ExpressionAttributeValues=expression_values,
+            ReturnValues="UPDATED_NEW"
+        )
+        return response
+    except ClientError as e:
+        raise e
 
 def delete_document(document_id: str) -> None:
-    table.delete_item(Key={'id': document_id})
-    return None
+    try: 
+        table.delete_item(Key={'id': document_id})
+        return None
+    except ClientError as e:
+        raise e
